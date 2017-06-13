318704c6e0fdbca15e3e70095549dac025ed09e6
mortalcatalyst <saythjwrenshaw@outlook.com>
2017-06-13 23:53:29 +1000

    removed next from router get

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
+
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.send('respond with a resource');
 });
 
-router.get('/cool', (req, res, next) => {
+router.get('/cool', (req, res) => {
   res.render('index', { title: 'Users are cool' });
 });
 
